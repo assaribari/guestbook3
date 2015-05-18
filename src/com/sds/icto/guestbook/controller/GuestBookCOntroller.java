f6ac026789946ed88ac345869801800badc47605
assaribari <assariba@gmail.com>
2015-05-18 17:23:48 +0900

    guestbook3

---
+++ src/com/sds/icto/guestbook/controller/GuestBookCOntroller.java
@@ -0,0 +1,47 @@
+package com.sds.icto.guestbook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sds.icto.guestbook.dao.GuestBookDao;
+import com.sds.icto.guestbook.vo.GuestBookVo;
+
+
+@Controller
+public class GuestBookCOntroller {
+
+	@Autowired
+	GuestBookDao GuestBookDao;
+	
+	@RequestMapping("/index")
+	public String index(Model model) {
+		List<GuestBookVo> list = GuestBookDao.fetchList();
+		model.addAttribute("list", list);
+		
+		return "/views/index.jsp";
+	}
+	
+
+	@RequestMapping(value= "/insert", method=RequestMethod.POST)
+	public String insert( @RequestParam("name") String name, 
+			@RequestParam("pass") String password,
+			@RequestParam("content") String message)
+			{
+
+		GuestBookVo vo = new GuestBookVo();
+		vo.setName(name);
+		vo.setPassword(password);
+		vo.setMessage(message);
+		
+		GuestBookDao.insert(vo);
+		return "redirect: /index";
+	
+	}
+	
+}
